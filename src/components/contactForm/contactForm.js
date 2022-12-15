8c268ef1a53a3ce7b7ee10f0503f810fa28eec81
gelomm <gelomendenilla@gmail.com>
2022-12-15 13:08:33 +0800

    added form input errors

--- src/components/contactForm/contactForm.js
+++ src/components/contactForm/contactForm.js
@@ -12,6 +12,21 @@ const ContactForm = () => {
         email: "",
         message: ""
     })
+    const [formErr, setFormErr] = useState({
+        nameErr: "",
+        emailErr: "",
+        messageErr: "",
+    })
+    const [inputErr, setInputErr] = useState({
+        inputNameErr: "",
+        inputEmailErr: "",
+        inputMessageErr: "",
+        
+        nameStyleErr: "",
+        emailStyleErr: "",
+        messageStyleErr: "",
+    })
+
     const [isSubmit, setIsSubmit] = useState(false)
 
     const handleName = (e) =>{
@@ -20,6 +35,14 @@ const ContactForm = () => {
             name: formVal.name = e.target.value
         }
         setFormVal(newVal)
+
+        if (formVal.name.length > 0 ) {
+            setFormErr({ ...formErr, nameErr: formErr.nameErr = ""})
+                setInputErr({ ...inputErr, 
+                    inputNameErr: inputErr.inputNameErr = "",
+                    nameStyleErr: inputErr.nameStyleErr = ""
+                })
+        }
     }
     const handleEmail = (e) =>{
         const newVal = {
@@ -27,6 +50,14 @@ const ContactForm = () => {
             email: formVal.email = e.target.value
         }
         setFormVal(newVal)
+
+        if (formVal.email.length > 0 ) {
+            setFormErr({ ...formErr, emailErr: formErr.emailErr = ""})
+                setInputErr({ ...inputErr, 
+                    inputEmailErr: inputErr.inputEmailErr = "",
+                    emailStyleErr: inputErr.emailStyleErr = ""
+                })
+        }
     }
     const handleMessage = (e) =>{
         const newVal = {
@@ -34,43 +65,77 @@ const ContactForm = () => {
             message: formVal.message = e.target.value
         }
         setFormVal(newVal)
+
+        if (formVal.message.length > 0 ) {
+            setFormErr({ ...formErr, messageErr: formErr.messageErr = ""})
+                setInputErr({ ...inputErr, 
+                    inputMessageErr: inputErr.inputMessageErr = "",
+                    messageStyleErr: inputErr.messageStyleErr = ""
+                })
+        }
     }
 
     const form = useRef();
 
     const sendEmail = (e) => {
     e.preventDefault();
+    
+    const email = () =>{
+        emailjs
+            .sendForm(
+                'service_1bw8w0n', 
+                'template_f2qco1i', 
+                form.current, 'mMpzcEMxAOtmXwtfY')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+            setIsSubmit(true)
+    
+            setTimeout(() => {
+                const newVal = {
+                    name: formVal.name = "",
+                    email: formVal.email = "",
+                    message: formVal.message = ""
+                }
+                setFormVal(newVal)
+    
+                setIsSubmit(false)
+            }, 2000);
+    }
 
-    emailjs
-        .sendForm(
-            'service_1bw8w0n', 
-            'template_f2qco1i', 
-            form.current, 'mMpzcEMxAOtmXwtfY')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        
-        // const newVal = {
-        //     name: formVal.name = "",
-        //     email: formVal.email = "",
-        //     message: formVal.message = ""
-        // }
-        // setFormVal(newVal)
-        setIsSubmit(true)
 
-        setTimeout(() => {
-            const newVal = {
-                name: formVal.name = "",
-                email: formVal.email = "",
-                message: formVal.message = ""
-            }
-            setFormVal(newVal)
+        //errors
+        if (formVal.name == "") {
+            setFormErr({ ...formErr, nameErr: formErr.nameErr = "name-error"})
+            setInputErr({ ...inputErr, 
+                inputNameErr: inputErr.inputNameErr = "Please add your name.",
+                nameStyleErr: inputErr.nameStyleErr = "input-name-error"
+            })
+        }
 
-            setIsSubmit(false)
-        }, 2000);
+        if (formVal.email == "") {
+            setFormErr({ ...formErr, emailErr: formErr.emailErr = "email-error"})
+            setInputErr({ ...inputErr, 
+                inputEmailErr: inputErr.inputEmailErr = "Please add your email.",
+                emailStyleErr: inputErr.emailStyleErr = "input-email-error"
+            })
+        }
+
+        if (formVal.message == "") {
+            setFormErr({ ...formErr, messageErr: formErr.messageErr = "message-error"})
+            setInputErr({ ...inputErr, 
+                inputMessageErr: inputErr.inputMessageErr = "Please add your message.",
+                messageStyleErr: inputErr.messageStyleErr = "input-message-error"
+            })
+        }
+        if (formVal.name != "" && formVal.email != "" && formVal.message != "") {
+            email();
+        }
+
     };
+
 
     return (
         <>
@@ -87,15 +152,18 @@ const ContactForm = () => {
                     <div>
                         <div>
                             <label>Name</label>
-                            <input value={formVal.name} onChange={handleName} type="text" name="user_name" />
+                            <input className={inputErr.nameStyleErr} value={formVal.name} onChange={handleName} type="text" name="user_name" />
+                            <span className={formErr.nameErr}>{inputErr.inputNameErr}</span>
                         </div>
                         <div>
                             <label>Email</label>
-                            <input value={formVal.email} onChange={handleEmail} type="email" name="user_email" />
+                            <input className={inputErr.emailStyleErr} value={formVal.email} onChange={handleEmail} type="email" name="user_email" />
+                            <span className={formErr.emailErr}>{inputErr.inputEmailErr}</span>
                         </div>
                         <div>
                             <label>Message</label>
-                            <textarea value={formVal.message} onChange={handleMessage} name="message" />    
+                            <textarea className={inputErr.messageStyleErr} value={formVal.message} onChange={handleMessage} name="message" />    
+                            <span className={formErr.messageErr}>{inputErr.inputMessageErr}</span>
                         </div>
                     </div>
                     <input className='button-28' type="submit" value="Send" />
--- src/components/contactForm/success.js
+++ src/components/contactForm/success.js
@@ -5,7 +5,7 @@ const Success = ({name}) => {
 return (
     <div className='success-cont'>
         {/* <h1>Email Sent</h1> */}
-        <h3>Thank you for reaching out {name || "Juan"}.</h3>
+        <h3>Thank you for reaching out<span>{name || "Juan"}</span>.</h3>
         <h1>Your email has been sent!</h1>
     </div>
 )
