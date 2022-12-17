6ca0f4be290a3355880184226412dee1c33e6347
gelomm <gelomendenilla@gmail.com>
2022-12-17 09:22:12 +0800

    project & skills animation change

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,10 +3,13 @@ import "../components/Cards/CardItem.css";
 import projectConfig from "../assets/configs/projectConfig"
 import "../assets/images/sklearn_genetic_opt.png"
 import React from 'react'
+import Aos from "aos";
+import 'aos/dist/aos.css';
 
 const Projects = () => {
+    Aos.init();
     return (
-        <div className="wrapper">
+        <div className="wrapper" data-aos="zoom-in-up">
             {projectConfig.map((item) => (
                 <CardItem item={item}/>
                 )
--- src/pages/Skills/Skills.js
+++ src/pages/Skills/Skills.js
@@ -3,9 +3,12 @@ import {AttentionSeeker} from "react-awesome-reveal";
 import SkillsItems from "./SkillsItems";
 import skillsConfig from "../../assets/configs/skillsConfig";
 import "./Skills.css"
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 
 const Skills = () => {
+    Aos.init();
     return (
         <section id="skills">
             <div>
@@ -13,20 +16,20 @@ const Skills = () => {
                     <h1 className="main-skills-h1">
                         <span className="main-skills"><strong>Main Skills & Tools</strong></span>
                     </h1>
-                    <AttentionSeeker effect={"bounce"}>
+                    <div data-aos="zoom-in-up">
                         <div className="main-skills">
                             <SkillsItems config={skillsConfig.mainSkills}/>
                         </div>
-                    </AttentionSeeker>
+                    </div>
 
                     <h1 className="complementary-skills-h1">
                         <span className="complementary-skills"><strong>Complementary Skills & Tools</strong></span>
                     </h1>
-                    <AttentionSeeker effect={"bounce"}>
+                    <div data-aos="zoom-in-up">
                         <div className="complementary-skills">
                             <SkillsItems config={skillsConfig.complementarySkills}/>
                         </div>
-                    </AttentionSeeker>
+                    </div>
                 </div>
             </div>
         </section>
